48f0dda3df02dba3ceb9ed98547d73625d5da2c5
tobil476 <tobilodigiani01@gmail.com>
2025-04-01 14:35:03 +0200

    namespace fix 2

--- src/Console/FilamentMediaManagerInstall.php
+++ src/Console/FilamentMediaManagerInstall.php
@@ -3,49 +3,10 @@
 namespace Tobil476\FilamentMediaManager\Console;
 
 use Illuminate\Console\Command;
-use Tobil476\ConsoleHelpers\Traits\RunCommand;
+//use Tobil476\ConsoleHelpers\Traits\RunCommand;
 
 class FilamentMediaManagerInstall extends Command
 {
-    use RunCommand;
-
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $name = 'filament-media-manager:install';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'install package and publish assets';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->info('Publish Vendor Assets');
-
-        \Artisan::call('vendor:publish', [
-            '--tag' => 'medialibrary-migrations'
-        ]);
-
-        \Artisan::call('migrate');
-
-        \Artisan::call('optimize:clear');
-
-        $this->info('Filament Media Manager installed successfully.');
-    }
+    //use RunCommand;
+    
 }
